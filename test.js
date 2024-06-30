aa1c9c6a81b7f968746aeb8079d1ae9d622a25db
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-06-30 20:11:54 -0300

    first component

---
+++ test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Test(){
+    
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+//to create the SearchMovies component
+//form with a class of form
+//label with htmlFor="query" and a class of Label
+//input of type text with a name of "query" and a placeholder
